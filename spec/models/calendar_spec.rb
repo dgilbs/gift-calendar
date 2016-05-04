0f660b48b4da0471a7bcefde449cf1da8de088c5
dgilbs <danielgilberg@gmail.com>
2016-05-04 12:09:23 -0400

    edits some tests

--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -1,24 +1,24 @@
-# require_relative '../rails_helper'
+require_relative '../rails_helper'
 
-# describe Calendar do 
+describe Calendar do 
 
-#   before(:each) do
-#     @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
-#     @valid_calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
-#     @invalid_calendar=Calendar.new(description: "My calendar!")
-#   end 
+  before(:each) do
+    @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
+    @valid_calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
+    @invalid_calendar=Calendar.new(description: "My calendar!")
+  end 
 
-#   it 'creates a new calendar' do
-#     expect(@valid_calendar.class).to eq(Calendar)
-#   end
+  it 'creates a new calendar' do
+    expect(@valid_calendar.class).to eq(Calendar)
+  end
 
-#   it 'creates a calendar that belongs to a user' do
-#     expect(@valid_calendar.user).to eq(@user)
-#   end
+  it 'creates a calendar that belongs to a user' do
+    expect(@valid_calendar.user).to eq(@user)
+  end
 
-#   it 'only creates a calendar with valid attributes' do
-#     expect(@valid_calendar).to be_valid
-#     expect(@invalid_calendar).to be_invalid
-#   end
+  it 'only creates a calendar with valid attributes' do
+    expect(@valid_calendar).to be_valid
+    expect(@invalid_calendar).to be_invalid
+  end
     
-# end
+end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,37 +1,47 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-#   describe Event do 
-#     before(:each) do
-#     @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
-#     @calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
-#     @event1=@calendar.events.build(name: "Dad's birthday", date: '2016-04-02', notes: "maybe a book?")
-#     @event2=@calendar.events.build(name: "Mom's birthday", date: '2016-04-30')
-#     @event3=Event.new(date: '2016-03-23')
-#     @event4=Event.new(name: "My Anniversary")
-#     end
+  describe Event do 
 
-#     it 'creates a event with valid attributes' do 
-#       expect(@event1.class).to eq(Event)
-#     end
+    it 'creates a event with valid attributes' do
+      @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
+      @calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
+      @event1=@calendar.events.build(name: "Dad's birthday", date: '2016-04-02', notes: "maybe a book?")
+      expect(@event1.class).to eq(Event)
+    end
 
-#     it 'is valid without notes' do 
-#       expect(@event2).to be_valid
-#     end
+    it 'is valid without notes' do 
+      @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
+      @calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
+      @event2=@calendar.events.build(name: "Mom's birthday", date: '2016-04-30')     
+      expect(@event2).to be_valid
+    end
 
-#     it 'is invalid without a name' do 
-#       expect(@event3).to be_invalid
-#     end
+    it 'is invalid without a name' do 
+      @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
+      @calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
+      @event3=Event.new(date: '2016-03-23')  
+      expect(@event3).to be_invalid
+    end
 
-#     it 'is invalid without a date' do 
-#       expect(@event4).to be_invalid
-#     end
+    it 'is invalid without a date' do 
+      @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
+      @calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
+      @event4=Event.new(name: "My Anniversary")      
+      expect(@event4).to be_invalid
+    end
 
-#     it 'belongs to a calendar' do 
-#       expect(@event1.calendar).to eq(@calendar)
-#     end 
+    it 'belongs to a calendar' do 
+      @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
+      @calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
+      @event1=@calendar.events.build(name: "Dad's birthday", date: '2016-04-02', notes: "maybe a book?")      
+      expect(@event1.calendar).to eq(@calendar)
+    end 
 
-#     it 'is belongs to a user' do
-#       expect(@user.events).to include(@event1) 
-#     end
+    it 'belongs to a user' do
+      @user=User.create(name: "Joe", email:"joe@yahoo.com", password: "ilovegifts")
+      @calendar=@user.calendars.build(name: "Birthdays", description: "People's Birthdays")
+      @event1=@calendar.events.build(name: "Dad's birthday", date: '2016-04-02', notes: "maybe a book?")     
+      expect(@event.user).to eq(@user) 
+    end
 
-# end 
+end 
