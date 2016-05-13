008be69e68ab620f3e1d68c2b6bd06392ffc8feb
dgilbs <danielgilberg@gmail.com>
2016-05-13 00:50:53 -0400

    works on Facebook authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = @user.id
     flash[:success] = "Welcome, #{@user.name}!"
-    redirect_to user_path(@user)
+    redirect_to calendars_path
   end
 
 
