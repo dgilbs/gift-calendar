57897d020cf530c687ea8c4c71da47db37ffd593
dgilbs <danielgilberg@gmail.com>
2016-05-25 17:48:30 -0400

    tinkers with some styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,12 +41,10 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth_hash)
     
     @user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+    byebug
     @user.name = auth_hash['info']['name']
     @user.email = auth_hash['info']['email']
     @user.password = " "
-    # user.location = auth_hash['info']['location']
-    # user.image_url = auth_hash['info']['image']
-    # user.url = auth_hash['info']['urls'][user.provider.capitalize]
     @user.save
     @user
   end
